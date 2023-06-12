9fafda5fb3df6ec66557659434437d9466311245
Andres Parra <andeveling@gmail.com>
2023-06-12 15:36:06 -0500

    fix title and logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -15,10 +15,14 @@ export default function NavBar() {
           : "bg-white/0"
       } z-30 transition-all`}
     >
-      <nav className="flex items-center justify-between h-20 max-w-screen-xl mx-5 xl:mx-auto">
-        <Link href="/" className="flex items-center text-2xl font-display">
-          <p className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent  drop-shadow-sm">
-            Arqustik V 1.0
+      <nav className="flex items-center justify-between max-w-screen-xl mx-5 h-28 xl:mx-auto">
+        <Link
+          href="/"
+          className="flex items-center text-2xl font-display indicator"
+        >
+          <span className="indicator-item badge badge-primary">Alfa</span>
+          <p className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent  drop-shadow-sm">
+            Arqustik <br /> Quoter
           </p>
         </Link>
         <QuoteDocumentIcon />
